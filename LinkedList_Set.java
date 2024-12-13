14809b09374c12b9b87245f797f1219f13784738
Ramya <ramyareddym023@gmail.com>
2024-12-13 11:02:42 +0530

    Add files via upload

---
+++ LinkedList_Set.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class LinkedList_Set {
+    public static void main(String args[])
+    {
+        LinkedList<String> a=new LinkedList<>();
+        a.add("Hello");
+        a.add("Hello");
+        a.add("Student");
+        a.add("Ram");
+        a.add("Student");
+        System.out.println(a);
+        //Removing an item from set
+        a.remove("Ram");
+        System.out.println(a);
+        //Clearing Linked list set
+        a.clear();
+        System.out.println(a);
+    }
+    
+}
