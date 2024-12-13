b2267fe4d36095cc1b2283b239c6617ac01b26a3
Ramya <ramyareddym023@gmail.com>
2024-12-13 12:41:46 +0530

    Add files via upload

---
+++ Printing_Using_Iterator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Printing_Using_Iterator {
+    public static void main(String args[])
+    {
+        LinkedHashSet<String> a=new LinkedHashSet<>();
+        a.add("hello");
+        a.add("hello");
+        a.add("Ram");
+        a.add("Sitha");
+        a.add("Ram");
+        //Printing using iterator
+        Iterator itr=a.iterator();
+        while(itr.hasNext())
+        {
+            System.out.println(itr.next()+" ");
+        }
+    }
+}
