c989683ec91fda5b939d3258ce60c1922f593044
Ramya <ramyareddym023@gmail.com>
2024-12-13 14:04:48 +0530

    Add files via upload

---
+++ Hash_Map.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hash_Map {
+    public static void main(String args[])
+    {
+        HashMap<String,String> a=new HashMap<> ();
+        a.put("sno","01");
+        a.put("sname","Ram");
+        System.out.println(a);
+    }
+}
